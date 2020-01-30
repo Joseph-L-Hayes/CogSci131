b85bbb196be966bad0f53e3ae5a8e5a5df27b448
Joseph Hayes <joseph.hayes@berkeley.edu>
2020-01-29 22:30:12 -0800

    still working on graph labels

--- warmup.py
+++ warmup.py
@@ -3,50 +3,52 @@ import matplotlib.pyplot as plt
 import math
 #
 # #problem 1
-#
-# def xPowSin(x):
-#     return np.sin(pow(2,x))
-#
-#
-# x = np.linspace(-1, 10, 10000)
-# y = xPowSin(x)
-#
-# plt.plot(x, y)
-# plt.ylim(-1.25, 1.25)
-# plt.show()
-#
-# #problem 2
-# x = np.linspace(0, 100, 10000)
-# g = xPowSin(x) / (xPowSin(x + 0.1))
-#
-# plt.ylim(-100, 100)
-# plt.xlim(0, 200)
-#
-# plt.plot(g, 'red')
-# plt.show()
+def xPowSin(x):
+    return np.sin(pow(2,x))
+
+
+x = np.linspace(-1, 10, 10000)
+y = xPowSin(x)
+
+plt.plot(x, y)
+plt.ylim(-1.25, 1.25)
+plt.show()
+
+#problem 2
+x = np.linspace(0, 100, 10000)
+g = xPowSin(x) / (xPowSin(x + 0.1))
+
+plt.ylim(-100, 100)
+plt.xlim(0, 200)
+
+plt.plot(g, 'red')
+plt.show()
 # #could be smoother? Needs better scaling
 #
-# #problem 3a
-# """returns the harmonic of k"""
-# def harmonic(k):
-#     return 1 / (k + 1)
-#
-# """returns the harmonic series sum up to n"""
-# def harmonicSum(n):
-#     total = 0
-#     for i in n:
-#         total = total + harmonic(i)
-#
-#     return total
-# #finished
-#
-# #problem 3b
-# x = np.linspace(1, 100, 1000)
-# y = harmonic(x)
-# plt.plot(x, y)
-# plt.show()
-#
-# #finished, check scaling
+#problem 3a
+"""returns the harmonic of k"""
+def harmonic(k):
+    return 1 / (k + 1)
+
+"""returns the harmonic series sum up to n"""
+def harmonicSum(n):
+    total = 0
+    for i in n:
+        total = total + harmonic(i)
+
+    return total
+#finished
+
+#problem 3b
+x = np.linspace(1, 100, 1000)
+y = harmonic(x)
+plt.title('Harmonic sum of n=1 to 100')
+plt.xlabel('n')
+plt.ylabel('1/1+i')
+plt.plot(x, y)
+plt.show()
+
+#finished, check scaling
 
 """plots a histogram of func with sample type of numSamples"""
 def histoPlot(func, sampleType, mean=0, stdev=1, numSamples=None, scale=None):
@@ -58,12 +60,18 @@ def histoPlot(func, sampleType, mean=0, stdev=1, numSamples=None, scale=None):
     plt.show()
 
 #problem 4
+plt.title('sin from standard normal distribution')
+plt.xlabel('x')
+plt.ylabel('sin(x)')
 histoPlot(np.sin, np.random.normal, 0, 1, 10000, 100)
-#finished
+
 
 #problem 5
+plt.title('Exponential of uniform distribution [0, 1.5]')
+plt.xlabel('x')
+plt.ylabel('e^x')
 histoPlot(np.exp, np.random.uniform, 0, 1.5, 10000, 100)
-#check graph shape!
+
 
 #problem 6a
 def getDiff(array): #may need to rewrite
