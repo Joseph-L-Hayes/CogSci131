1c11efc6b91fe84ce3209fda081180a7e03cbc0e
Joseph Hayes <joseph.hayes@berkeley.edu>
2020-02-01 14:01:00 -0800

    writing doctests for RescorlaWagner function

--- ResWagModel.py
+++ ResWagModel.py
@@ -1,10 +1,15 @@
 import numpy as np
 import matplotlib as plt
+import doctest
+from decimal import *
 
 def RescorlaWagner(associationStr, t, salience=0.5, learnRate=0.1):
     """ Returns the association strength of a stimulant after t time steps
-    >>>RescorlaWagner(.2, 10, .5, .1)
+    >>> RescorlaWagner(0.2, 1, .5, .1)
+    ([0.2, 0.24], array([1]), 0.24)
 
+    >>> RescorlaWagner(.5, 10, .5, .1)
+    ([.5, ], array([1]), last)
 
     """
     Vs = [associationStr]
@@ -12,12 +17,13 @@ def RescorlaWagner(associationStr, t, salience=0.5, learnRate=0.1):
 
     for trial in trials:
         associationStr += salience * learnRate * (1 - associationStr)
+        associationStr = round(associationStr, 2)
         Vs.append(associationStr)
 
     return Vs, trials, associationStr
 
 
-
+doctest.testmod()
 
 
 #end
