b8b05789ac790e188d3024e7df44a1b2c5fa8794
Joseph Hayes <joseph.hayes@berkeley.edu>
2020-02-18 15:44:33 -0800

    backup

--- elizaTutorial.py
+++ elizaTutorial.py
@@ -18,12 +18,15 @@ def response(s1, s2):
     z = re.search(r"My name is (.+)", s2)
 
     one = m.group(1) #string matches pattern, one is the first part of match
+    two = m.group(2)
+    three = z.group(1)
     # three = m.group(3) #will error if no group exists
 
     #go over randomizing the adjective for the response
 
-    if m:
-        print('Hello %s, it is a pleasure to meet you!' % one)
+    if m and z:
+        print('Hello %s the %s, it is a pleasure to meet you!' % (one, two))
+        print('Hello %s, it is a pleasure to meet you!' % three)
         # print('Group 1:', m.group(1))
         # print('Group 2:', m.group(2))
 
@@ -34,7 +37,6 @@ def response(s1, s2):
     #     print(z.group(1))
     #groups are the (.+) portions of the string
     #this allows you to pick out parts of the string you may want to work with
-
 
 def responseAll(*args):
 
@@ -71,7 +73,7 @@ def running2(): #try using regex matching for response
 
 
 # response('My name is ELIZA the chatbot', 'My name is Joe')
-running('How', 'many', 'miles', 'can', 'you', 'run?')
+# running('How', 'many', 'miles', 'can', 'you', 'run?')
 # running('miles')
 #
 # responseAll('My name is ELIZA the chatbot', 'My name is Joe')
@@ -100,6 +102,55 @@ running('How', 'many', 'miles', 'can', 'you', 'run?')
 #         print(x)
 #
 # printString('I', 'would', 'like', 'to', 'learn', 'more', 'about', 'python')
+
+""" Only refer to things below here. """
+def ruleOne(string):
+
+    pets = ['dog', 'cat', 'fish']
+    cars = ['Honda', 'Chevy']
+    nouns = {}
+
+    nouns.update(pet = pets)
+    nouns.update(car = cars)
+
+    for x, y in nouns.items():
+        if string in nouns[x]:
+            print('I have a %s too' % string)
+
+def ruleTwo(string):
+    return False
+
+def ruleThree(string):
+    return False
+
+
+# for pet in pets:
+#     print(pet)
+def takeQuestion():
+    rules = {1: ruleOne, 2:ruleTwo, 3:ruleThree}
+    number = random.randint(1, 3)
+    print(number)
+
+
+    s = input('What kind of pet do you have?')
+    rules[number](s)
+
+
+takeQuestion()
+# def test():
+
+
+"""To uncomment the above after #, highlight the text and use command + /"""
+
+
+
+
+
+
+
+
+
+
 
 
 #end
