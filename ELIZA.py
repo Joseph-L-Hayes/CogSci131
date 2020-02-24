1706354cec35ed907ad1a2fc0e9ae1b63689c1f0
Joseph Hayes <joseph.hayes@berkeley.edu>
2020-02-24 11:59:11 -0800

    created file and started ELIZA class, randomRule function, and takeQuestion functions

--- ELIZA.py
+++ ELIZA.py
@@ -1,8 +1,28 @@
 import re
+import random
+
+class ELIZA:
+
+    def __init__(self):
+        self.name = ELIZA.takeQuestion()
+
+    def takeQuestion():
+        """Takes in initial question from terminal"""
+        return input('Hello, my name is ELIZA. What is your name?')
+
+    def randomRule(self, ruleList):
+        """Takes in a list of rules and returns one at random"""
+        number = random.randint(0, len(ruleList) - 1 )
+
+        return ruleList[number]
+
+
+
+
 
 #project 4
-
-
+for i in range(50):
+    print(ELIZA().randomRule(list))
 
 
 
