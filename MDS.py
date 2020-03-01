9ff577117d67f2f624828860316402beab459b5a
Joseph Hayes <joseph.hayes@berkeley.edu>
2020-03-01 10:32:43 -0800

    created mds.py and template code for stress and gradient functions

---
+++ MDS.py
@@ -0,0 +1,17 @@
+import numpy as np
+import random
+
+def stress(vector):
+    """Returns the stress of vector positions"""
+    return None
+
+def gradient(vector):
+    """Returns the gradient of vector positions"""
+    return None
+
+
+
+
+
+
+#end
