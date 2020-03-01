1352727711ccf7bd5e2301b59fa73923a6fa49d4
Joseph Hayes <joseph.hayes@berkeley.edu>
2020-03-01 12:02:08 -0800

    backup

--- MDS.py
+++ MDS.py
@@ -2,9 +2,22 @@ import numpy as np
 import random
 
 
-def importCSV(csvFile, dataType, header=1):
+def importCSV(csvFile, dataType, header=1): #how to reference the sports without headers?
+    """Returns a numpy array without headers from a CSV file"""
     return np.genfromtxt(csvFile, dtype=dataType ,delimiter=",", skip_header=1)
 
+def simToDist(x):
+    return 1 - x
+
+
+def convertArray(simArray, func):
+    """Takes a similarity array and converts it to a distance array using the supplied
+        function func"""
+    array = np.array(21, dtype=float)
+    for x in range(21):
+        array[x] = np.apply_along_axis(func, x, simArray) #errors
+
+    return array
 
 def stress(vector):
     """Returns the stress of vector positions"""
@@ -14,10 +27,11 @@ def gradient(vector):
     """Returns the gradient of vector positions"""
     return None
 
-
-
-sportArray = importCSV("similarities.csv", float)
-print(sportArray)
+array = importCSV('similarities.csv', float)
+print(array)
+print(array.shape)
+convertArray(array, simToDist)
+print(array)
 
 
 #end
