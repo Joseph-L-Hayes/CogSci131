2713898398c67b233d8bef6ab718adf0515fa2ce
Joseph Hayes <joseph.hayes@berkeley.edu>
2020-03-09 10:10:00 -0700

    created functions contains and generateRegions

--- Generalization.py
+++ Generalization.py
@@ -3,4 +3,11 @@ import numpy as np
 import matplotlib as plt
 
 
-def contains(region, ):
+def generateRegions(numRegions, low, high):
+    #numpy.random.uniform(low=0.0, high=1.0, size=None)
+    regions = [sorted((np.random.uniform(low, high), np.random.uniform(low, high))) for n in range(numRegions)]
+    return regions
+
+def contains(region, point):
+    """Takes a region tuple and returns if point is in the region"""
+    return (region[0] <= point) and (region[1] >= point)
