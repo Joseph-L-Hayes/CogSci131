ea9ccb6a59dbc303e1f64cc78bf99e750d8d40e0
Joseph Hayes <joseph.hayes@berkeley.edu>
2020-03-10 21:08:17 -0700

    backup

--- Generalization.py
+++ Generalization.py
@@ -75,10 +75,26 @@ def plotProb(z, interval, regions, saveName=None, scale='linear'):
 
 # regionList = generateRegions(10000, -10, 10)
 # plotProb(0, 10, regionList, 'a6_p3')
+# plotProb(0, 10, regionList, scale='log')
 
-"""Problem 4: """
+"""Problem 4: One way to check if the curve has an exponential decrease is to plot a
+    logarithmic y axis and look for a straight line. Why does this check if the curve
+    is exponential?
+    ANSWER: """
 
+"""Problem 5: Plot Q3 with a logarithmic y axis for x ranging from -5 to 5, and x
+    ranging from -10 to 10. What do these two plots show? How do you interpret them?
+    Explain in a few sentences."""
 
+"""Problem 6: In previous questions, we've been assuming that people implement the law
+    perfectly and we have been trying to approximate their behavior using 10,000 regions.
+    However, people themselves have limited resources. What if people themselves only used
+    a few consequential regions in order to compute generalizations? Re-plot Question 2
+    using only 10, 100, and 1000 consequential regions. What patterns do you see?"""
+
+"""Problem 7: Describe a way you could test how many consequential regions people actually
+    made use of in this kind of generalization. Could you tell the difference between 10 and
+    10,000? Could you tell the difference between 10,000 and 20,000, why or why not?"""
 
 
 
