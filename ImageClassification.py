9652cfba264b72fd6bd96f647cb5234eecd8919e
Joseph Hayes <joseph.hayes@berkeley.edu>
2020-03-13 11:44:03 -0700

    working on perceptron code

--- ImageClassification.py
+++ ImageClassification.py
@@ -34,5 +34,54 @@ assert N == DIM[0]*DIM[1] # just check our sizes to be sure
 # set up some random initial weights
 weights = np.random.normal(0,1,size=N)
 
+# np.savetxt('0_data.csv', A, delimiter=',')
+# np.savetxt('1_data.csv', B, delimiter=',')
 
 ## Your code here:
+"""Problem 1: Write an implementation of the perceptron learning algorithm that first
+    loads images for the digit “0” and then for the digit “1”. Start with random weights
+    from a normal distribution. Compute the average accuracy on blocks of 25 items and plot
+    this accuracy until you think it won’t get better."""
+
+def perceptron(data_set, weights, trials): #seems like updating the weights to improve accuracy only
+    #I'm guessing we process 25 blocks at a time and for '0' we should get some
+    #level of accuracy as num1/25 or the positive over total because we know this
+    #data is from a 0 training set.
+    products = []
+    for i in range(trails): #start by computing current classification
+        products += [weights[i] * data_set[i]]
+
+    total = sum(products)
+
+    return total, weights
+
+def prediction(x):
+    if x >= 0:
+        return 1
+    else:
+        return 0
+
+def update(x, w):
+
+    return None
+
+numTrials = 25
+perceptron(A, weights, numTrials)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#end
