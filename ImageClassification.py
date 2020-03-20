8c2f509d3bc49c196cec86aef4469c8d8689b25f
Joseph Hayes <joseph.hayes@berkeley.edu>
2020-03-19 21:57:55 -0700

    fixed issue with problem 4 plot going from 100% to 0%. The labels weren't changing between digits. Labels are now chosen at random and expected behavior is seen.

--- ImageClassification.py
+++ ImageClassification.py
@@ -168,32 +168,39 @@ class Perceptron(object):
         if plot:
             self.plot_accuracy(xAxis, accuracy_trace)
 
-# zeroOnePercept = Perceptron(N, trainingImages, 4, 9)
-# beforeWeight = np.copy(zeroOnePercept.get_weights())
+# zeroOnePercept = Perceptron(N, trainingImages, 0, 1)
 # zeroOnePercept.train(.97, 5, 25, plot=True)
-# print(zeroOnePercept.overall_accuracy) #will give the last accuracy number aka overall_accuracy
-# zeroOneWeight = np.copy(zeroOnePercept.get_weights())
 #
-# np.save('pre_zero_one', beforeWeight)
-# np.save('zero_one', zeroOneWeight)
+# np.save('zero_one_weights', zeroOnePercept.get_weights())
 
 """Problem 2: Does your solution in Q1 converge on 100% accuracy or not? What does this
     mean in terms of the linear separability of “0” and “1” on this feature space?
 
-    ANSWER: INCOMPLETE"""
+    ANSWER: My solution for Q1 approaches 100% (I used the method of total correct
+    /total images) which reaches .9999 for digits 0 and 1. This tells me that 0 and 1
+    are completely linearly separable. The pair have unique features that allow for
+    distinguishing them completely. """
 
 """Problem 3: What do large negative and large positive values mean, intuitively? What
     do numbers near zero mean? Why does this matrix look the way that it does, in terms
     of where large positive and negative terms are located?
 
-    ANSWER: INCOMPLETE """
+    ANSWER: The large positive and negative values seem to occur where the digits were
+    in the training images. For my perceptron, 1 took very large values while 0 took
+    very negative values. In my matrix, the general shape of a 0 can be seen in black
+    which is the color for large negative values and bright yellow can be seen in the center
+    of the 0 where a 1 would be if the digits overlapped. There is also yellow outside the
+    black 0 shape and I take this to represent some of the 1 images that were written in
+    a 'forward slash' shape. Numbers near 0 represent the 0's (as opposed to the 1's) from
+    the training images where the weights were updated but the feature values in that area
+    was 0 so the weights stayed small."""
 
-def weightMatrix(weights, dims, save=False, fileName='TITLE',method=None, bounds=[0, 1]):
+def weightMatrix(weights, dims, save=False, fileName='FILENAME',method=None, bounds=[0, 1]):
     # weights= weights[10]
     weights = weights.reshape(dims, dims)
-    fig, im = plt.subplots(figsize=(10, 10))
-    wm = plt.imshow(weights, interpolation=ir.interpol_methods[method], cmap=plt.get_cmap(ir.color_maps[ir.color_maps.index('inferno')]), vmin=bounds[0], vmax=bounds[1])
-
+    fig, im = plt.subplots(figsize=(10, 10)) #
+    wm = plt.imshow(weights) #, vmin=bounds[0], vmax=bounds[1]
+    #cmap=plt.get_cmap(ir.color_maps[ir.color_maps.index('inferno')]), interpolation=ir.interpol_methods[method],
     wm.axes.get_xaxis().set_visible(False)
     wm.axes.get_yaxis().set_visible(False)
     plt.title('Heat Map of Trained Weight Matrix For Digits 0 and 1', fontsize=15)
@@ -203,8 +210,8 @@ def weightMatrix(weights, dims, save=False, fileName='TITLE',method=None, bounds
 
     plt.show()
 
-# boundList = [-1, 1]
-# weightMatrix(np.load('zero_one.npy'), 28, method=4, bounds=boundList)
+# boundList = [0, 1]
+# weightMatrix(np.load('zero_one_weights.npy'), 28, save=True, fileName='a7p3_norm_bias', method=0, bounds=boundList)
 
 """Problem 4: What should you expect to happen if you set the elements of the weight vector
     which are close to zero to be actually zero? Do this for the 10, 20, 30, ... 780 weight
@@ -212,22 +219,28 @@ def weightMatrix(weights, dims, save=False, fileName='TITLE',method=None, bounds
     classifications of “0” vs “1”. What does this tell you about the proportion of the image
     which is diagnostic about “0” vs “1”?
 
-    ANSWER: INCOMPLETE"""
+    ANSWER: Setting the weights 'close' to 0 should negate the effect of those elements; they
+    shouldn't contribute to the classification of the digits. My plot showed near 100% classification
+    accuracy up to about 120 elements set to 0 and then the accuracy falls to and hovers around 50%.
+    When the accuracy falls to 50% the perceptron is essentially guessing as it doesn't have enough
+    feature data to know which digit is which.
+    """
 
-def testClassification(numIters, index, digit0, digit1, perceptron, unseenDict, weights):
+def testClassification(numIters, digit0, digit1, perceptron, unseenDict, weights):
     numCorrect = 0
 
     for i in range(numIters):
-        result = perceptron.classify(weights, unseenDict[index][i % (numIters // 2)])
+        label = random.choice([digit0, digit1])
+        result = perceptron.classify(weights, unseenDict[label][i % (numIters // 2)])
 
-        if index == digit0 and result == 0:
+        if label == digit0 and result == 0:
             numCorrect += 1
-            index = digit1
-        elif index == digit1 and result == 1:
+            label = digit1
+        elif label == digit1 and result == 1:
             numCorrect += 1
-            index = digit0
+            label = digit0
 
-    return numCorrect, index
+    return numCorrect
 
 def setToZero(trainDict, unseenDict, N, digit0, digit1):
     accuracyList = []
@@ -248,30 +261,38 @@ def setToZero(trainDict, unseenDict, N, digit0, digit1):
             modWeights[index] = 1000
             k += 1
 
-        result, index = testClassification(1000, digit0, digit0, digit1, perceptron01, unseenDict, perceptron01.weights)
+        result = testClassification(1000, digit0, digit1, perceptron01, unseenDict, perceptron01.weights)
         accuracyList += [result / 1000]
         weightsList += [np.copy(perceptron01.weights.reshape(28, 28))]
 
     return accuracyList, xList, weightsList
 
-accList, xList, moddedWeights = setToZero(trainingImages, unseenImages, N, 0, 1)
-print(accList)
-plt.plot(xList, accList)
-plt.title('Modified Weight Values at Intervals 10, 20, 30, ..., 780')
-plt.ylabel('Average Accuracy')
-plt.xlabel('Weight Values Changed to 0 (smallest first)')
+# accList, xList, moddedWeights = setToZero(trainingImages, unseenImages, N, 0, 1)
+# plt.plot(xList, accList)
+# plt.title('Modified Weight Values at Intervals 10, 20, 30, ..., 780')
+# plt.ylabel('Average Accuracy')
+# plt.xlabel('Weight Values Changed to 0 (smallest first)')
 # plt.savefig('a7p4.pdf')
-plt.show()
-plt.close()
-
-# plt.imshow(moddedWeights[11])
+# plt.show()
+# plt.close()
+#
+# plt.imshow(moddedWeights[10])
 # plt.savefig('mod_at_11.pdf')
+# plt.close()
+# plt.imshow(moddedWeights[13])
+# plt.savefig('mod_at_13.pdf')
+# plt.close()
+
 
 """Problem 5: Next show a matrix of the classification accuracy of each pair of digits
     after enough training. Make this a plot (with colors for accuracy rather than numbers).
     Does it match your intuitions about which pairs should be easy vs. hard? Why or why not?
 
-    ANSWER: INCOMPLETE
+    ANSWER: The matrix does approximately match my intuition. All digits compared with
+    themselves (0 and 0 for example) have 100% accuracy. The perceptron is 96 - 100%
+    accurate for most pairs. I would have expected (4, 9), (3, 5), and (5, 8) to be harder
+    to linearly separate due to the amount of overlap these digits tend to have in handwriting
+    and my matrix reflects this intuition with 90-92% accuracy for these digit pairs.
     """
 
 def allDigitsAcc(trainDict, unseenDict, N):
@@ -285,40 +306,39 @@ def allDigitsAcc(trainDict, unseenDict, N):
             correct = 0
             percept = Perceptron(N, trainDict, k, j)
             percept.train(.97, 5, 25)
-            index = k
-
-            result, index = testClassification(1000, index, k, j, percept, unseenDict, percept.get_weights())
+            #remove label, does nothing, needed to mod for P4!
+            result = testClassification(1000, k, j, percept, unseenDict, percept.get_weights())
             digitArray[k, j] = result / 1000
 
     return digitArray, xlabels, ylabels
 
 #uncomment below to run code for Problem 5
-# grid, xAx, yAx = allDigitsAcc(trainingImages, unseenImages, N)
-# np.save('accMatrix', grid)
-# np.save('accMatrix_X', xAx)
-# np.save('accMatrix_Y', yAx)
+grid, xAx, yAx = allDigitsAcc(trainingImages, unseenImages, N)
+np.save('accMatrix', grid)
+np.save('accMatrix_X', xAx)
+np.save('accMatrix_Y', yAx)
 
-# grid = np.load('accMatrix.npy')
-# xAx = np.load('accMatrix_X.npy')
-# yAx = np.load('accMatrix_Y.npy')
-#
-# plt.rcParams['xtick.bottom'] = plt.rcParams['xtick.labelbottom'] = False
-# plt.rcParams['xtick.top'] = plt.rcParams['xtick.labeltop'] = True
-#
-# fig, ax = plt.subplots() #figsize=(10,10)
-# matrix = plt.imshow(grid, cmap='inferno', extent=[0, 9, 9, 0]) #'coolwarm'
-# figure_title = 'Classification Accuracy For Each Pair of Digits'
-# plt.text(0.5, 1.13, figure_title,
-#          horizontalalignment='center',
-#          fontsize=12,
-#          transform = ax.transAxes)
-# fig.colorbar(matrix, orientation='horizontal', fraction=.05) #, ticks=[.50, .60, .70, .80, .90, 1]
-# ax.set_xticks(xAx)
-# ax.set_yticks(yAx)
-#
-# plt.savefig('a7p5accuracy_3.pdf')
-#
-# plt.show()
+grid = np.load('accMatrix.npy')
+xAx = np.load('accMatrix_X.npy')
+yAx = np.load('accMatrix_Y.npy')
+
+plt.rcParams['xtick.bottom'] = plt.rcParams['xtick.labelbottom'] = False
+plt.rcParams['xtick.top'] = plt.rcParams['xtick.labeltop'] = True
+
+fig, ax = plt.subplots() #figsize=(10,10)
+matrix = plt.imshow(grid, cmap='inferno', extent=[0, 9, 9, 0]) #'coolwarm'
+figure_title = 'Classification Accuracy For Each Pair of Digits'
+plt.text(0.5, 1.13, figure_title,
+         horizontalalignment='center',
+         fontsize=12,
+         transform = ax.transAxes)
+fig.colorbar(matrix, orientation='horizontal', fraction=.05) #, ticks=[.50, .60, .70, .80, .90, 1]
+ax.set_xticks(xAx)
+ax.set_yticks(yAx)
+
+plt.savefig('a7p5accuracy_4.pdf')
+
+plt.show()
 
 
 #end
