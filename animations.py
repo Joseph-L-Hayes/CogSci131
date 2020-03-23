7e50c5721cf4d6eb59ba6a5a277c61fb959be36b
Joseph Hayes <joseph.hayes@berkeley.edu>
2020-03-23 16:22:41 -0700

    saved videos of training weights

--- animations.py
+++ animations.py
@@ -28,9 +28,6 @@ def loadImages(files, show=False, unseen=False):
 
 fileNames = ['Zero', 'One', 'Two', 'Three', 'Four', 'Five', 'Six', 'Seven', 'Eight', 'Nine']
 
-trainingImages, unseenImages = loadImages(fileNames, unseen=False)
-
-
 class Perceptron(object):
 
     def __init__(self, weights, dimensions, data, digit0, digit1):
@@ -39,10 +36,6 @@ class Perceptron(object):
         self.digits = [digit0, digit1]
         self.bias = np.zeros(dimensions)
         self.overall_accuracy = 0
-
-    def get_weights(self):
-        #changed this to a copy when live plotting; may cause issues with other functions already written
-        return self.weights
 
     def dotProd(self, w, x):
 
@@ -56,31 +49,10 @@ class Perceptron(object):
 
         else: return 0
 
-    def classify(self, w, x):
-        dot = self.dotProd(w, x)
-
-        if dot >= 0:
-            return 1
-        else: return 0
-
     def random_label(self):
         """Returns a random dataset label"""
         labels = list(self.data_set.keys())
         return random.choice(labels)
-
-    def plot_accuracy(self, x, y, save=False, name=None, color='red'):
-
-        plt.xlabel('Blocks of 25')
-        plt.ylabel('Accuracy')
-        plt.title('P1: Training Accuracy for Digits {0} and {1}'.format(self.digits[0], self.digits[1]))
-        plt.legend(loc=0, title='Converged to {0} Accuracy in {1} Blocks'.format(round(y[-1], 5), x[-1]))
-        plt.plot(x, y, c=color, label='')
-
-        if save:
-            plt.savefig(name + '.pdf')
-
-        plt.show()
-        plt.close()
 
     def aniTrain(self):
 
@@ -88,7 +60,6 @@ class Perceptron(object):
         data = self.data_set[label]
         sub_data = random.choice(data)
         y = self.predict(self.weights, sub_data, self.bias)
-        oldData = np.copy(self.weights)
 
         if label == 0 and y == 1:
             self.bias -= sub_data
@@ -97,9 +68,6 @@ class Perceptron(object):
             self.bias += sub_data
             self.weights += sub_data + self.bias
 
-        if np.sum(oldData == self.weights) == 0:
-            return
-
         return self.weights.reshape(28, 28)
 
 # np.save('animationWeights.npy', np.random.normal(0, 1, size=784))
@@ -107,6 +75,34 @@ digit0, digit1 = 1, 0
 trainingImages, unseenImages = loadImages(fileNames, unseen=False)
 percept = Perceptron(np.load('animationWeights.npy'), 784, trainingImages, digit0, digit1)
 
+
+def updatefig(*args):
+    global im, numIters
+    im.set_array(percept.aniTrain())
+    numIters += 1
+    plt.xlabel('Training iterations: ' + str(numIters))
+
+    return im,
+
+Writer = animation.writers['ffmpeg']
+writer = Writer(fps=15, metadata=dict(artist='JS'), bitrate=1800)
+
+numIters = 0
+fig = plt.figure(figsize=(5, 5))
+im = plt.imshow(percept.aniTrain(), animated=True, interpolation=ir.interpol_methods[9], cmap=plt.get_cmap(ir.color_maps[ir.color_maps.index('tab10')])) #, cmap=plt.get_cmap(ir.color_maps[ir.color_maps.index('inferno')])
+# im.axes.get_xaxis().set_visible(False)
+im.axes.get_yaxis().set_visible(False)
+#put aniTrain as function in imshow, first arg
+# fig.colorbar(im, orientation='horizontal', fraction=.0415)
+plt.title('Perceptron Training For Digits ' + str(digit0) + ' and ' + str(digit1), fontsize=10)
+ani = animation.FuncAnimation(fig, updatefig, frames=2500, interval=10, blit=True)
+ani.save('1-0_weight_training_FUNKS.mp4', writer=writer)
+plt.show()
+
+
+
+
+#Scrap?
 # class AnimateMatrix(object):
 #     def __init__(self, perceptron, digit0, digit1):
 #         self.perceptron = perceptron
@@ -136,22 +132,3 @@ percept = Perceptron(np.load('animationWeights.npy'), 784, trainingImages, digit
 #
 # model = AnimateMatrix(percept, digit0, digit1)
 # model.animateMatrix()
-
-def updatefig(*args):
-    global im, numIters
-    im.set_array(percept.aniTrain())
-    numIters += 1
-    plt.xlabel('Training iterations: ' + str(numIters))
-
-    return im,
-
-numIters = 0
-fig = plt.figure(figsize=(5, 5))
-im = plt.imshow(percept.aniTrain(), animated=True, interpolation=ir.interpol_methods[9], cmap=plt.get_cmap(ir.color_maps[ir.color_maps.index('inferno')])) #, cmap=plt.get_cmap(ir.color_maps[ir.color_maps.index('inferno')])
-# im.axes.get_xaxis().set_visible(False)
-im.axes.get_yaxis().set_visible(False)
-#put aniTrain as function in imshow, first arg
-# fig.colorbar(im, orientation='horizontal', fraction=.0415)
-plt.title('Perceptron Training For Digits ' + str(digit0) + ' and ' + str(digit1), fontsize=10)
-ani = animation.FuncAnimation(fig, updatefig, frames=1, interval=10, blit=False)
-plt.show()
